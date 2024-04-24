0129f3c8969c54ad83c348fe58431cbd862ef3b8
ElaineTavares <etsets.tavares@gmail.com>
2024-04-24 14:01:56 -0300

    24/04/24

--- js/index.js
+++ js/index.js
@@ -1,21 +1,22 @@
 // /////////////////////////////////////MENU HAMBURGER/////////////////////////////////
 let menuMobile = document.querySelector('.menu_mobile')
 let menuMobileIcone = document.querySelector('.menu_mobile_img')
+let ulNav = document.querySelector('.ul_nav')
 
-function toggleMenu(){
-    let ulNav = document.querySelector('.ul_nav')
+function toggleMenu(){ 
     ulNav.classList.toggle('active'); 
     if(ulNav.classList.contains('active')){
         menuMobileIcone.setAttribute('src', '../imagens/menu_hamburger.svg')
     } else{
         menuMobileIcone.setAttribute('src', '../imagens/menu_x.svg')
     }
+    
 }
-
-
 menuMobile.addEventListener('click', toggleMenu);
 
+
 // /////////////////////////////FECHA MENU HAMBURGER /////////////////////////////////
+
 
 
 //////////////////////////////////////DATA ANIME////////////////////////////////////////////////////////////
